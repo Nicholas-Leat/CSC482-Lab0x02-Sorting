37885ab8e74a37f1f601643577f0f1726aba503d
Nick Leat <nick.leat@trojans.dsu.edu>
2020-09-22 12:14:10 -0500

    Finished setting up files

---
+++ CSC482-Sorting/src/com/company/CSC482Sorting.java
@@ -0,0 +1,35 @@
+package com.company;
+
+public class CSC482Sorting {
+    public static void main(String[] args) {
+        // write your code here
+        int N,k,d;
+        N = 10;
+        k = 6;
+        //byte[] myList = GenerateTestList;
+        //myList = SelectionSort(myList);
+        System.out.println("1");
+
+
+    }
+    public static byte[] GenerateTestList(int N, int k, int minV, int maxV){
+        byte[] myList = new byte[N];
+
+
+        return myList;
+    }
+    public static byte[] SelectionSort(byte[] sortList){
+        return sortList;
+    }
+    public static byte[] MergeSort(byte[] sortList){
+        return sortList;
+    }
+    public static byte[] QuickSort(byte[] sortList){
+        return sortList;
+    }
+    public static byte[] RadixSort(byte[] sortList){
+        return sortList;
+    }
+
+
+}
